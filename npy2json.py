283c14f5b5891a90e180bf268e46ce49049fcb54
Avaer Kazmer <a@modules.io>
2022-10-09 13:16:54 -0400

    Update npy2json.py

--- npy2json.py
+++ npy2json.py
@@ -1,6 +1,11 @@
 import sys
 import numpy
+import json
 
 filename = sys.argv[1]
 npy = numpy.load(filename, allow_pickle=True)
 npy0 = npy.item(0);
+s = json.dumps({
+    'motion': npy0['motion'].tolist(),
+})
+print(s)
