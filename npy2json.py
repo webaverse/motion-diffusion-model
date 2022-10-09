9151251f779f0d9be429dcc86d8d46435b375468
Avaer Kazmer <a@modules.io>
2022-10-09 13:18:54 -0400

    Update npy2json.py

--- npy2json.py
+++ npy2json.py
@@ -7,5 +7,9 @@ npy = numpy.load(filename, allow_pickle=True)
 npy0 = npy.item(0);
 s = json.dumps({
     'motion': npy0['motion'].tolist(),
+    'text': npy0['text'],
+    'lengths': npy0['lengths'],
+    'num_samples': npy0['num_samples'],
+    'num_repetitions': npy0['num_repetitions'],
 })
 print(s)
